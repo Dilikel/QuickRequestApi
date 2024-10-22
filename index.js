6b3af9ebd8a2459d3ec2629b6ddf707ae4581b3b
vovan101207 <shiryayev_vova@bk.ru>
2024-10-22 17:59:16 +0300

    1.1

--- index.js
+++ index.js
@@ -2,15 +2,17 @@ import express from 'express'
 import dotenvConfig from './config/dotenv.config.js'
 import connectDB from './config/db.js'
 import authRoutes from './routes/authRoutes.js'
+import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT || 8000
 
 connectDB()
 
+app.use(cors())
 app.use(express.json())
 
-app.use('/auth', authRoutes)
+app.use(authRoutes)
 
 app.listen(PORT, () => {
 	console.log(`Сервер запущен`)
